df97c6ef413ea240b7f1dbd073489e24728da463
gvensan <4477169+gvensan@users.noreply.github.com>
2025-01-17 18:13:42 +0530

    Briing in latest data generation logic

--- examples/index.js
+++ examples/index.js
@@ -1,236 +1,289 @@
-import { generateRandomPayload, generateRandomTopic } from '../index.js';
+import { generateEventAndTopic } from '../index.js';
 
-const event = {
-  "topic": "acmeretail/oracleRetailOrder/INSERT/V1/{CHANNEL}/{SALES_REP_ID}/{ORDER_ID}",
-  "eventName": "Insert Orders",
-  "eventVersion": "0.1.0",
-  "messageName": "Insert_Orders",
+const data =  {
+  "topic": "sap/erp/businesspartner/create/V1/{businessPartnerType}/{partnerId}",
   "topicParameters": {
-      "CHANNEL": {
-          "schema": {
-              "type": "string"
-          },
-          "rule": {
-              "name": "CHANNEL",
-              "type": "string",
-              "group": "StringRules",
-              "rule": "alpha",
-              "casing": "mixed",
-              "minLength": 10,
-              "maxLength": 10
-          }
+    "partnerId": {
+      "schema": {
+        "type": "string"
       },
-      "ORDER_ID": {
-          "schema": {
-              "type": "string"
-          },
-          "rule": {
-              "name": "ORDER_ID",
-              "type": "string",
-              "group": "StringRules",
-              "rule": "alpha",
-              "casing": "mixed",
-              "minLength": 10,
-              "maxLength": 10
-          }
-      },
-      "SALES_REP_ID": {
-          "schema": {
-              "type": "string"
-          },
-          "rule": {
-              "name": "SALES_REP_ID",
-              "type": "string",
-              "group": "StringRules",
-              "rule": "alpha",
-              "casing": "mixed",
-              "minLength": 10,
-              "maxLength": 10
-          }
+      "rule": {
+        "name": "partnerId",
+        "type": "string",
+        "group": "StringRules",
+        "rule": "alpha",
+        "casing": "mixed",
+        "minLength": 10,
+        "maxLength": 10
       }
+    },
+    "businessPartnerType": {
+      "schema": {
+        "type": "string"
+      },
+      "rule": {
+        "name": "businessPartnerType",
+        "type": "string",
+        "group": "StringRules",
+        "rule": "enum",
+        "enum": [
+          "PARTNER1",
+          "PARTNER2",
+          "PRTNER3"
+        ]
+      }
+    }
   },
+  "eventName": "Business Partner Create",
+  "eventVersion": "0.1.0",
+  "messageName": "Business_Partner_Create",
+  "hasPayload": true,
   "payload": {
-      "CHANNEL": {
-          "type": "string",
-          "enum": [
-              "ONLINE",
-              "IN STORE",
-              "VENDING MACHINE"
-          ],
-          "rule": {
+    "businessPartner": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "firstName": {
+            "type": "string",
+            "rule": {
+              "name": "firstName",
               "type": "string",
-              "enum": [
-                  "ONLINE",
-                  "IN STORE",
-                  "VENDING MACHINE"
-              ],
-              "name": "CHANNEL",
               "group": "StringRules",
-              "rule": "enum"
-          }
-      },
-      "ORDER_ID": {
-          "type": "number",
-          "rule": {
-              "name": "ORDER_ID",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "int",
-              "minimum": 0,
-              "maximum": 1000
-          }
-      },
-      "ORDER_DATE": {
-          "format": "date-time",
-          "type": "string",
-          "rule": {
-              "name": "ORDER_DATE",
+              "rule": "alpha",
+              "casing": "mixed",
+              "minLength": 10,
+              "maxLength": 10
+            }
+          },
+          "lastName": {
+            "type": "string",
+            "rule": {
+              "name": "lastName",
               "type": "string",
-              "group": "DateRules",
-              "rule": "recent",
-              "days": 1
-          }
-      },
-      "QUANTITY": {
-          "type": "number",
-          "rule": {
-              "name": "QUANTITY",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "int",
-              "minimum": 0,
-              "maximum": 10
-          }
-      },
-      "CUSTOMER_ID": {
-          "type": "number",
-          "rule": {
-              "name": "CUSTOMER_ID",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "int",
-              "minimum": 0,
-              "maximum": 1000
-          }
-      },
-      "DELIVERY_ADDRESS_ID": {
-          "type": "number",
-          "rule": {
-              "name": "DELIVERY_ADDRESS_ID",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "int",
-              "minimum": 0,
-              "maximum": 1000
-          }
-      },
-      "STATE": {
-          "type": "number",
-          "enum": [
-              "Created"
-          ],
-          "rule": {
-              "name": "STATE",
-              "type": "number",
               "group": "StringRules",
-              "rule": "enum",
-              "enum": [
-                  "Created"
-              ]
+              "rule": "alpha",
+              "casing": "mixed",
+              "minLength": 10,
+              "maxLength": 10
+            }
+          },
+          "addressLink": {
+            "type": "array",
+            "items": {
+              "type": "object",
+              "properties": {
+                "address": {
+                  "type": "array",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "country": {
+                        "type": "string",
+                        "rule": {
+                          "name": "country",
+                          "type": "string",
+                          "group": "StringRules",
+                          "rule": "alpha",
+                          "casing": "mixed",
+                          "minLength": 10,
+                          "maxLength": 10
+                        }
+                      },
+                      "nation": {
+                        "type": "string",
+                        "rule": {
+                          "name": "nation",
+                          "type": "string",
+                          "group": "StringRules",
+                          "rule": "alpha",
+                          "casing": "mixed",
+                          "minLength": 10,
+                          "maxLength": 10
+                        }
+                      },
+                      "city": {
+                        "type": "string",
+                        "rule": {
+                          "name": "city",
+                          "type": "string",
+                          "group": "StringRules",
+                          "rule": "alpha",
+                          "casing": "mixed",
+                          "minLength": 10,
+                          "maxLength": 10
+                        }
+                      },
+                      "street": {
+                        "type": "string",
+                        "rule": {
+                          "name": "street",
+                          "type": "string",
+                          "group": "StringRules",
+                          "rule": "alpha",
+                          "casing": "mixed",
+                          "minLength": 10,
+                          "maxLength": 10
+                        }
+                      },
+                      "postalCode": {
+                        "type": "string",
+                        "rule": {
+                          "name": "postalCode",
+                          "type": "string",
+                          "group": "StringRules",
+                          "rule": "alpha",
+                          "casing": "mixed",
+                          "minLength": 10,
+                          "maxLength": 10
+                        }
+                      },
+                      "houseNumber": {
+                        "type": "string",
+                        "rule": {
+                          "name": "houseNumber",
+                          "type": "string",
+                          "group": "StringRules",
+                          "rule": "alpha",
+                          "casing": "mixed",
+                          "minLength": 10,
+                          "maxLength": 10
+                        }
+                      }
+                    }
+                  },
+                  "subType": "object",
+                  "name": "address",
+                  "rule": {
+                    "name": "address",
+                    "type": "object"
+                  }
+                },
+                "addressNumber": {
+                  "type": "string",
+                  "rule": {
+                    "name": "addressNumber",
+                    "type": "string",
+                    "group": "StringRules",
+                    "rule": "alpha",
+                    "casing": "mixed",
+                    "minLength": 10,
+                    "maxLength": 10
+                  }
+                },
+                "dateFrom": {
+                  "type": "number",
+                  "rule": {
+                    "name": "dateFrom",
+                    "type": "number",
+                    "group": "NumberRules",
+                    "rule": "float",
+                    "minimum": 0,
+                    "maximum": 1000,
+                    "fractionDigits": 2
+                  }
+                }
+              }
+            },
+            "subType": "object",
+            "name": "addressLink",
+            "rule": {
+              "name": "addressLink",
+              "type": "object"
+            }
+          },
+          "partnerId": {
+            "type": "string",
+            "rule": {
+              "name": "partnerId",
+              "type": "string",
+              "group": "StringRules",
+              "rule": "alpha",
+              "casing": "mixed",
+              "minLength": 10,
+              "maxLength": 10
+            }
+          },
+          "businessPartnerType": {
+            "type": "string",
+            "rule": {
+              "name": "businessPartnerType",
+              "type": "string",
+              "group": "StringRules",
+              "rule": "alpha",
+              "casing": "mixed",
+              "minLength": 10,
+              "maxLength": 10
+            }
           }
+        }
       },
-      "SALES_REP_ID": {
-          "type": "number",
-          "rule": {
-              "name": "SALES_REP_ID",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "int",
-              "minimum": 0,
-              "maximum": 1000
-          }
-      },
-      "INVOICE_ADDRESS_ID": {
-          "type": "number",
-          "rule": {
-              "name": "INVOICE_ADDRESS_ID",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "int",
-              "minimum": 0,
-              "maximum": 1000
-          }
-      },
-      "ORDER_TOTAL": {
-          "type": "number",
-          "rule": {
-              "name": "ORDER_TOTAL",
-              "type": "number",
-              "group": "NumberRules",
-              "rule": "float",
-              "minimum": 0,
-              "maximum": 50,
-              "fractionDigits": 2
-          }
+      "subType": "object",
+      "name": "businessPartner",
+      "rule": {
+        "name": "businessPartner",
+        "type": "object"
       }
+    }
   },
   "publishSettings": {
-      "count": 0,
-      "interval": 1000,
-      "delay": 0
+    "count": 0,
+    "interval": 1000,
+    "delay": 0
   },
   "mappings": [
-      {
-          "type": "Topic Parameter",
-          "source": {
-              "type": "Payload Parameter",
-              "name": "CHANNEL",
-              "fieldName": "CHANNEL",
-              "fieldType": "string"
-          },
-          "target": {
-              "type": "Topic Parameter",
-              "name": "CHANNEL",
-              "fieldName": "CHANNEL",
-              "fieldType": "string"
-          }
+    {
+      "type": "Payload Parameter",
+      "source": {
+        "type": "Topic Parameter",
+        "name": "partnerId",
+        "fieldName": "partnerId",
+        "fieldType": "string"
       },
-      {
-          "type": "Topic Parameter",
-          "source": {
-              "type": "Payload Parameter",
-              "name": "ORDER_ID",
-              "fieldName": "ORDER_ID",
-              "fieldType": "number"
-          },
-          "target": {
-              "type": "Topic Parameter",
-              "name": "ORDER_ID",
-              "fieldName": "ORDER_ID",
-              "fieldType": "string"
-          }
-      },
-      {
-          "type": "Topic Parameter",
-          "source": {
-              "type": "Payload Parameter",
-              "name": "SALES_REP_ID",
-              "fieldName": "SALES_REP_ID",
-              "fieldType": "number"
-          },
-          "target": {
-              "type": "Topic Parameter",
-              "name": "SALES_REP_ID",
-              "fieldName": "SALES_REP_ID",
-              "fieldType": "string"
-          }
+      "target": {
+        "type": "Payload Parameter",
+        "name": "businessPartner[0].partnerId",
+        "fieldName": "partnerId",
+        "fieldType": "string"
       }
+    },
+    {
+      "type": "Payload Parameter",
+      "source": {
+        "type": "Payload Parameter",
+        "name": "businessPartner[0].addressLink[0].address[0].nation",
+        "fieldName": "nation",
+        "fieldType": "string"
+      },
+      "target": {
+        "type": "Payload Parameter",
+        "name": "businessPartner[0].addressLink[0].address[0].country",
+        "fieldName": "country",
+        "fieldType": "string"
+      }
+    },
+    {
+      "type": "Topic Parameter",
+      "source": {
+        "type": "Payload Parameter",
+        "name": "businessPartner[0].addressLink[0].address[0].country",
+        "fieldName": "country",
+        "fieldType": "string"
+      },
+      "target": {
+        "type": "Topic Parameter",
+        "name": "partnerId",
+        "fieldName": "partnerId",
+        "fieldType": "string"
+      }
+    }
   ]
+};
+
+const events = generateEventAndTopic({rule: data, count: 1});
+
+for (var i=0; i<events.length; i++) {
+  console.log('---- Event[' + i + '] ----');
+  console.log('Payload:', JSON.stringify(events[i].payload, null, 4));
+  console.log('Topic:', events[i].topic);
 }
-
-
-const payload = generateRandomPayload(event.payload);
-const topic = generateRandomTopic(event, payload);
-
-console.log('Payload:', payload);
-console.log('Topic:', topic);
